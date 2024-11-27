3b0ec4da48dbe78aee6579071d3522c8523fd489
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 08:29:35 +0700

    Delete Bai3/Cau3_1.py

--- Bai3/Cau3_1.py
+++
@@ -1,7 +0,0 @@
-
-Bước 1: Thực hiện tạo mới window form và các menu theo code mẫu
-Bước 2: Tiến hành các thay đổi cần thiết để nhận được kết quả các cửa sổ window
-có các menu như hình:
-
-Bước 3: Thêm các phương thức OpenFile(), Exit(), InsText(), InsPic() thực hiển
-hiển thị các thông báo hiển thị ra màn hình các thông báo lựa chọn tương ứng
