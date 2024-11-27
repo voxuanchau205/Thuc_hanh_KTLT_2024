bc2b3059ab4723bc81b3ee3f442172cfabf4b8c0
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 09:17:10 +0700

    Create Cau6_8.py

---
+++ Bài6/Cau6_8.py
@@ -0,0 +1,22 @@
+from search_module import Sequential_Search
+* Enter the number of elements
+n = int(input (Enter the number of elements in the list: "))
+* Enter the elements of the list
+dlist[]
+for i in range(n)
+    element int(input("decimal (1+1)))
+    diies.append(element)
+* Enter the element to search for
+Item = int (input("element array to find: "))
+* Search for elements
+found, index= Sequential_Search(dlist, item)
+# Print the results
+if found
+    print("item found at index).")
+else:
+    print("The (item) was not found in the list.")
+
+
+
+
+
