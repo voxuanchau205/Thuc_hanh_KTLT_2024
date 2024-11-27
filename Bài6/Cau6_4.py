45757565887bfcf7be226d43cf955fe674c66e0e
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 08:49:30 +0700

    Create Cau6_4.py

---
+++ Bài6/Cau6_4.py
@@ -0,0 +1,7 @@
+
+# Create an array with values ​​from 12 to 30 
+array= list (range (12, 38))
+print(" Constant created: "array)
+# Reverse
+reversed_array= array[::-1]
+print("Reversed constant: ", reversed_array)
