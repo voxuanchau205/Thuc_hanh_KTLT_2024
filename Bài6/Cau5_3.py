b038b8dad93427984421cc113f6ea62af72f956d
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 10:24:43 +0700

    Delete Bài6 directory

--- Bài6/Cau5_3.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-x= np.arange (12, 38)
-print(x)
--- Bài6/Cau5_8modul.py
+++
@@ -1,6 +0,0 @@
-
-def Sequential_Search(list, item)
-   for index, value in enumerate (dist):
-     if value==item:
-        return (True, Index)
-   return (False, -1)
