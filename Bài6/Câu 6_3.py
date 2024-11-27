aab08b183a488a7c0080362f00c8a9a31dc42f61
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 10:34:32 +0700

    Create Câu 6_3.py

---
+++ Bài6/Câu 6_3.py
@@ -0,0 +1,14 @@
+
+class Person (object):
+  def getGender (self):
+   return "Unknown"
+class Nam (People):
+  def getGender ( self ):
+return "Nam"
+class Nu (Human):
+  def getGender (self): 
+   return "Nữ"
+aNam = Nam()
+aNu= Nu ()
+print (aNam.getGender())
+print (aNu.getGender())
