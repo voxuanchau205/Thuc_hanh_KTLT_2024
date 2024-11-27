8c2bedd4d0aa8fa87b351042b49b65dfda528076
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 10:30:26 +0700

    Create Câu 6_2.py

---
+++ Bài6/Câu 6_2.py
@@ -0,0 +1,10 @@
+class HinhChunhat:
+  def _init_(self, length, width): 
+      self.length = length
+      self.height = height
+  def calculate_area(self):
+      return self.length self.width
+# Create Chunhat Image object
+image = hinhChunhat (70, 50)
+# Calculate area
+print (f"Area of ​​rectangle is: (shape.calc_area()}")
