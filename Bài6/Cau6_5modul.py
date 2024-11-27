7ab5838f184564ccd146b3ecc98e3cb4054d81cc
voxuanchau205 <babo31052005@gmail.com>
2024-11-27 09:07:12 +0700

    Create Cau6_5modul.py

---
+++ Bài6/Cau6_5modul.py
@@ -0,0 +1,10 @@
+
+def sort_list (lst):
+***sort the list in ascending order.***
+   return sorted ( lst)
+def find_max (Lst)
+***Find the largest element in the list.***
+   return max(lst)
+def find_min(lst):
+***Find the smallest element in the list.***
+  return min(lst)
