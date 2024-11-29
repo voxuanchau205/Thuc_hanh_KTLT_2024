fb5c7123788cc8d21bdd6a8267c8085151a4a610
voxuanchau205 <babo31052005@gmail.com>
2024-11-29 11:49:54 +0700

    Create Câu 6_6.py

---
+++ Bài6/Câu 6_6.py
@@ -0,0 +1,13 @@
+class StringManipulator:
+     def _init_(self):
+         self.input string =""
+     def get String (self):
+         self.input_string = input ("Enter string: ").strip()
+     def print_String (self):
+         print(self.input_string.upper())
+def main():
+     manipulator = StringManipulator()
+     manipulator.get_String()
+     manipulator.print_String()
+"if_name_=="_main_":
+     main() 
