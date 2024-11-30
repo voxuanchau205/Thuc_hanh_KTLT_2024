3e4c2d369fadbe64316040c25e9eb73e9a65a84e
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 19:45:57 +0700

    Create Câu4_15.py

---
+++ bài4/Câu4_15.py
@@ -0,0 +1,7 @@
+ds = input('enter string: ').split()
+#arrange words according to dictionary
+danh_sach=sorted (ds) 
+#print out words according to Dictionary
+print("words from dictionary: ")
+for ds in danh_sach:
+   print (ds)
