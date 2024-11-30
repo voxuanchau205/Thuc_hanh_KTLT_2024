5573f9a3d046c77b6101f6622fca5f40ffed9b67
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 12:10:53 +0700

    Update Cau2_1.py

--- Bai2/Cau2_1.py
+++ Bai2/Cau2_1.py
@@ -1,6 +1,7 @@
-Bước 1: Thực hiện tạo mới window form và các menu theo code mẫu
-Bước 2: Tiến hành các thay đổi cần thiết để nhận được kết quả các cửa sổ window
-có các menu như hình:
 
-Bước 3: Thêm các phương thức OpenFile(), Exit(), InsText(), InsPic() thực hiển
-hiển thị các thông báo hiển thị ra màn hình các thông báo lựa chọn tương ứng
+nl=int(input("nl nhập: "))
+n2=int(input("nhap n2: "))
+nếu nl>n2:
+   print("nl lon")
+else
+   print("n2 lon")
