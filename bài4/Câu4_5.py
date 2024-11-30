ff5b762c0e824f073f8061af681571d35d9c54d2
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 19:28:19 +0700

    Create Câu4_5.py

---
+++ bài4/Câu4_5.py
@@ -0,0 +1,8 @@
+ds=input(' list: ') split()
+# print the entire range just entered
+print (ds)
+#print the input data, one ptu per line
+for so in ds:
+#print string backwards
+  so =ds [::-1]
+  print(so)
