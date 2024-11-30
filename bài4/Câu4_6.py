b0f9da0bd32941de35fa38819806fad7ec0dfea7
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 19:30:20 +0700

    Create Câu4_6.py

---
+++ bài4/Câu4_6.py
@@ -0,0 +1,10 @@
+hoten-input("enter your full name :")
+slist=hoten.split()
+n=len(list)
+ho= slist [0]
+ten=slist[n-l]
+dem=" ".join(slist[l:n-l])
+print("your name is: ", ten)
+print("your last name is: ", ho)
+if n>2:
+     print("your middle name is: ",dem)
