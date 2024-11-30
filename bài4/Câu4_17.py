6f776e377b67f17de66ec085bd3871c204be0cf8
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 19:51:20 +0700

    Create Câu4_17.py

---
+++ bài4/Câu4_17.py
@@ -0,0 +1,10 @@
+def tong_cua_uoc_so(n):
+     for i in range(1, n):
+          tong_uoc=0
+          for j in range(1, i+1):
+              if i j ==0:
+               tong_uoc +=j
+     if tong_uoc> i:
+              print(i)
+n=int(input("enter positive integer n: ")) 
+tong_cua_uoc_so(n)
