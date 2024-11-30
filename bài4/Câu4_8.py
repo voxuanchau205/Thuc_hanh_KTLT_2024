2812218c2fb0c886a41a5239e939b5af993f14a4
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 19:36:17 +0700

    Create Câu4_8.py

---
+++ bài4/Câu4_8.py
@@ -0,0 +1,10 @@
+def tu dai_nhat(s):
+   #longest stored variable
+   tudainhat = ""
+   dscactus.split()
+   for tu in dscactu:
+       if (len(tu) > len (tudainhat)) or (len(tu) ==len (tudainhat) and tu< tudainhat):
+          tudainhat=tu
+   return tudainhat
+s=input()
+print (tu_dai_nhat(s))
