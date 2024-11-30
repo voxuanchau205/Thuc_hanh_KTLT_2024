29e8c7949f614973c908aac9dba54d9b7d774c63
voxuanchau205 <babo31052005@gmail.com>
2024-11-30 12:12:15 +0700

    Create Bài 2_2.py

---
+++ Bai2/Bài 2_2.py
@@ -0,0 +1,9 @@
+import math;
+xl=int(input("nhap x1--->"))
+yl-int (input("nhap yl--->"))
+x2=int(input("nhap x2--->"))
+y2=int(input("nhap y2--->"))
+d1 = (x2 - x1) * (x2 - x1);
+d2 = (y2 - yl) * (y2 - yl);
+res = math.sqrt(dl+d2)
+print("khoang cach giua 2 diem:",res)
